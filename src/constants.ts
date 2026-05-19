39667f05eec723576e972ac7a880dc7324de6dd3
rffm95 <sucessomacico@gmail.com>
2026-05-19 01:48:46 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,15 +6,15 @@
 import { ScheduleEntry } from './types';
 
 export const HORARIOS: ScheduleEntry[] = [
-  { dia: "Segunda", inicio: "12:00", fim: "20:00", funcionario: "Bento" },
-  { dia: "Segunda", inicio: "20:00", fim: "03:00", funcionario: "JP" },
+  { dia: "Segunda", inicio: "15:00", fim: "20:00", funcionario: "Dinis" },
+  { dia: "Segunda", inicio: "20:00", fim: "03:00", funcionario: "Bento" },
   { dia: "Segunda", inicio: "22:00", fim: "03:00", funcionario: "David" },
 
-  { dia: "Terça", inicio: "12:00", fim: "20:00", funcionario: "Bento" },
-  { dia: "Terça", inicio: "20:00", fim: "03:00", funcionario: "JP" },
-  { dia: "Terça", inicio: "22:00", fim: "03:00", funcionario: "David" },
+  { dia: "Terça", inicio: "15:00", fim: "20:00", funcionario: "Dinis" },
+  { dia: "Terça", inicio: "20:00", fim: "03:00", funcionario: "David" },
+  { dia: "Terça", inicio: "22:00", fim: "03:00", funcionario: "Bento" },
 
-  { dia: "Quarta", inicio: "12:00", fim: "20:00", funcionario: "Bento" },
+  { dia: "Quarta", inicio: "12:00", fim: "20:00", funcionario: "Dinis" },
   { dia: "Quarta", inicio: "20:00", fim: "03:00", funcionario: "Divad" },
   { dia: "Quarta", inicio: "22:00", fim: "03:00", funcionario: "David" },
 
@@ -28,12 +28,12 @@ export const HORARIOS: ScheduleEntry[] = [
   { dia: "Sexta", inicio: "22:00", fim: "03:00", funcionario: "JP" },
 
   { dia: "Sábado", inicio: "12:00", fim: "20:00", funcionario: "Dinis" },
-  { dia: "Sábado", inicio: "20:00", fim: "03:00", funcionario: "Bento" },
-  { dia: "Sábado", inicio: "22:00", fim: "03:00", funcionario: "David" },
+  { dia: "Sábado", inicio: "20:00", fim: "03:00", funcionario: "David" },
+  { dia: "Sábado", inicio: "22:00", fim: "03:00", funcionario: "Rui" },
   { dia: "Sábado", inicio: "22:00", fim: "03:00", funcionario: "JP" },
 
-  { dia: "Domingo", inicio: "12:00", fim: "21:00", funcionario: "Dinis" },
-  { dia: "Domingo", inicio: "15:00", fim: "21:00", funcionario: "JP" }
+  { dia: "Domingo", inicio: "12:00", fim: "21:00", funcionario: "Bento" },
+  { dia: "Domingo", inicio: "15:00", fim: "21:00", funcionario: "Dinis" }
 ];
 
 export const DIAS = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"];
